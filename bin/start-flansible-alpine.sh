d82bbf5a16ad3f3379277460f4771fd6ac5e635e
dpilipovic <49248453+dpilipovic@users.noreply.github.com>
2019-04-10 14:21:21 -0700

    Update start-flansible-alpine.sh

--- bin/start-flansible-alpine.sh
+++ bin/start-flansible-alpine.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 cd /opt/flansible ;
 /opt/flansible/venv/bin/python /opt/flansible/venv/bin/gunicorn -b 0.0.0.0:8000 -k gevent --worker-connections 1000 --timeout 900 run:app -D;
 /opt/flansible/venv/bin/python /opt/flansible/venv/bin/gunicorn -b 0.0.0.0:8001 -k gevent --worker-connections 1000 --timeout 900 run:app -D;
